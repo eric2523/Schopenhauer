d18f48452ceb7b8677ae38c9fddbe8a2c748ac99
eric2523@gmail.com <eric2523@gmail.com>
2020-10-26 09:27:14 -0700

    Create configureStore file

---
+++ frontend/src/store/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+const configureStore = (preloadedState = {}) => {
+  let middleware = [thunk];
+  if (process.env.NODE_ENV !== "production") {
+    middleware = [...middleware, logger];
+  }
+
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(...middleware)
+  );
+};
+
+export default configureStore
