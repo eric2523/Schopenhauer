1fd1b0b6e4868035afb35a98728f7a2684e9c927
hgparker <haroldgparker@gmail.com>
2020-10-26 11:03:32 -0700

    Add validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,3 +8,5 @@ const User = require("../../models/User");
 
 const validateSignUpInput = require('../../validation/signup');
 const validateLoginInput = require('../../validation/login');
+
+module.exports = router;
