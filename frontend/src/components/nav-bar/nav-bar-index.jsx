4a5b901e6bf9bd04db5f6f9e2222d28a71186de8
eric2523@gmail.com <eric2523@gmail.com>
2020-10-26 10:51:46 -0700

    Prepare nav items components

--- frontend/src/components/nav-bar/nav-bar-index.jsx
+++ frontend/src/components/nav-bar/nav-bar-index.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import { NavBarItem } from './nav-bar-item'
-import { mainNavPaths } from './nav-paths'
+import React from "react";
+import { NavBarItem } from "./nav-bar-item";
+import { mainNavPaths } from "./nav-paths";
 
 export class NavBarIndex extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
 
-  render(){
-    const navPaths = mainNavPaths()
-    return(
-      <div>
-        We are in Nav Bar Index
-      </div>
-    )
+  render() {
+    const navPaths = mainNavPaths();
+    let navListItems = [];
+    for (const keys in navPaths) {
+      let { name, path } = navPaths[keys];
+
+      navListItems.push(
+        <NavBarItem 
+          name={name} 
+          path={path} 
+        />
+      );
+    }
+
+    return <div>We are in Nav Bar Index</div>;
   }
 }
-
