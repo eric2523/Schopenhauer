cd8c1549a7ca99c606257f566f59d1187b8d2d75
Nathan Chau <nateychau@gmail.com>
2020-10-26 21:32:53 -0700

    Set up button component for sound file upload

--- frontend/src/actions/song_actions.js
+++ frontend/src/actions/song_actions.js
@@ -2,9 +2,11 @@ export const RECEIVE_CURRENT_SONG = "RECEIVE_CURRENT_SONG";
 export const CLEAR_SONG = "CLEAR_SONG";
 
 export const receiveCurrentSong = (song) => {
+  debugger;
   return {
     type: RECEIVE_CURRENT_SONG,
-    song
+    //TESTING PURPOSES ONLY (WILL CALL FORMDATA METHODS IN API UTIL METHODS)
+    song: song.get('song[file]')
   }
 }
 
@@ -12,4 +14,11 @@ export const clearSong = () => {
   return {
     type: CLEAR_SONG
   }
+}
+
+
+//Need to implement thunk action creators after backend api endpoints are created
+
+export const uploadSong = (song) => (dispatch) => {
+  dispatch(receiveCurrentSong(song))
 }
--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -4,6 +4,9 @@ import { NavBarIndex } from "./nav-bar/nav-bar-index";
 import { Route } from "react-router-dom";
 import { Modal } from "./nav-bar/user_auth_modal";
 
+//Will probably move this component to a full sound bar component
+import { UploadButton } from './music_player/upload_button';
+
 export const App = () => {
   return (
     <div>
@@ -12,6 +15,7 @@ export const App = () => {
       </header>
       <Modal/>
       <Visualizer />
+      <UploadButton />
     </div>
   );
 };
