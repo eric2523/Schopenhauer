df595186220e080317218dea9fec87398552a512
eric2523@gmail.com <eric2523@gmail.com>
2020-10-28 11:18:13 -0700

    Add carousel header and styling

--- frontend/src/components/landing-page/carousel.jsx
+++ frontend/src/components/landing-page/carousel.jsx
@@ -8,16 +8,31 @@ class Carousel extends React.Component {
   }
 
   render() {
+    let carouselTitle = "Explore recent creations"
+    if (this.props.isAuthenticated){
+      carouselTitle = "Your recent creations"
+    }
+
     return (
-      <div>
-        Carousel
+      <div className="carousel-main-div">
+        <div className="carousel-header">
+          <h1 className="carousel-title">
+            {carouselTitle}
+          </h1>
+        </div>
+
+        <div className="carousel-items">
+          <CarouselItem />
+        </div>
       </div>
     );
   }
 }
 
 // eventualy need to map template slice of state into carousel
-const mSTP = (state) => ({});
+const mSTP = (state) => ({
+  isAuthenticated: state.session.isAuthenticated
+});
 
 // map fetching of recent/ user templates
 const mDTP = (dispatch) => ({});
