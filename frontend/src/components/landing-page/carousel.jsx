14e44057a16581f8cb5011b28cce8f63d12dba98
eric2523@gmail.com <eric2523@gmail.com>
2020-10-28 11:38:02 -0700

    Add pure-react-carousel library

--- frontend/src/components/landing-page/carousel.jsx
+++ frontend/src/components/landing-page/carousel.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
-import {CarouselItem} from "./carousel-item";
+import { CarouselItem } from "./carousel-item";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 class Carousel extends React.Component {
   constructor(props) {
@@ -8,21 +16,37 @@ class Carousel extends React.Component {
   }
 
   render() {
-    let carouselTitle = "Explore recent creations"
-    if (this.props.isAuthenticated){
-      carouselTitle = "Your recent creations"
+    let carouselTitle = "Explore recent creations";
+    if (this.props.isAuthenticated) {
+      carouselTitle = "Your recent creations";
     }
 
     return (
       <div className="carousel-main-div">
         <div className="carousel-header">
-          <h1 className="carousel-title">
-            {carouselTitle}
-          </h1>
+          <h1 className="carousel-title">{carouselTitle}</h1>
         </div>
 
         <div className="carousel-items">
-          <CarouselItem />
+          <CarouselProvider
+            naturalSlideWidth={100}
+            naturalSlideHeight={125}
+            totalSlides={3}
+          >
+            <Slider>
+              <Slide index={0}>
+                <CarouselItem />
+              </Slide>
+              <Slide index={1}>
+                <CarouselItem />
+              </Slide>
+              <Slide index={2}>
+                <CarouselItem />
+              </Slide>
+            </Slider>
+            <ButtonBack>Back</ButtonBack>
+            <ButtonNext>Next</ButtonNext>
+          </CarouselProvider>
         </div>
       </div>
     );
@@ -31,7 +55,7 @@ class Carousel extends React.Component {
 
 // eventualy need to map template slice of state into carousel
 const mSTP = (state) => ({
-  isAuthenticated: state.session.isAuthenticated
+  isAuthenticated: state.session.isAuthenticated,
 });
 
 // map fetching of recent/ user templates
