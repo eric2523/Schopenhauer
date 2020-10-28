f3643cc355690512b8a0626abce411de959547da
Nathan Chau <nateychau@gmail.com>
2020-10-27 23:58:54 -0700

    Add deployment configuration to app.js

--- app.js
+++ app.js
@@ -4,9 +4,21 @@
 const express = require("express");
 const app = express();
 
+//path for deployment
+const path = require('path');
+
 // Run locally on 5000, on variable if deployed
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+//index route for deployment
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 // Test to make sure Express is working
 app.get("/", (req, res) => res.send("My name is Arthur"));
