f7b9d270e5568d586d4c88d2038c73cd325f4c78
Yuehan <yuehan.huang@protonmail.com>
2020-10-28 12:57:46 -0700

    Add octave construction function

---
+++ frontend/src/components/visualizers/octave.js
@@ -0,0 +1,18 @@
+import { getFrequencyAmp } from "../../util/visualizer_util";
+
+export const octave = (frequencyArray, context) => {
+  const fmin = 27.5;
+  let freq = fmin;
+  const nyquist = context.sampleRate / 2;
+  const octaveSteps = 12;
+  const octaveAmp = new Array(octaveSteps).fill(0);
+  let i = 0;
+  while (freq < nyquist) {
+    freq = Math.pow(2, i / octaveSteps) * fmin;
+    octaveAmp[i % octaveSteps] +=
+      getFrequencyAmp(freq, frequencyArray, nyquist) / 50;
+    i++;
+  }
+
+  return octaveAmp;
+};
