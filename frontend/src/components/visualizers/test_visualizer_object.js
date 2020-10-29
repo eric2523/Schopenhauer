9591d080d3e96c36efbcd96f99e15ede24078c32
Yuehan <yuehan.huang@protonmail.com>
2020-10-28 21:25:35 -0700

    Create a test visualizer object

---
+++ frontend/src/components/visualizers/test_visualizer_object.js
@@ -0,0 +1,19 @@
+export const testVisualizer = {
+  type: "",
+  typeSettings: { binCount: 1024 },
+  generalSettings: {
+    //for canvas settings
+
+    centerX: 350,
+    centerY: 350,
+
+    //for any bar
+    barWidth: 1,
+
+    //for any circular object
+    radius: 0,
+
+    // finished controls
+    heightAmplifier: 0.5,
+  },
+};
