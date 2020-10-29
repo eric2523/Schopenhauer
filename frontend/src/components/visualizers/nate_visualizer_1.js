54d9e776f1be0f0f1c31b69b919cfec1e66fc726
Nathan Chau <nateychau@gmail.com>
2020-10-28 19:24:36 -0700

    Create sphere visualizer

---
+++ frontend/src/components/visualizers/nate_visualizer_1.js
@@ -0,0 +1,93 @@
+
+//represents one particle in sphere. Code inspired by Louis Hoebregts
+class SphereParticle {
+  constructor(canvas){
+
+    this.particleRadius = 4;
+    this.color = '#' + Math.floor(Math.random()*16777215).toString(16);
+    //canvas properties
+    this.canvas = canvas;
+    this.perspective = canvas.width * 0.8;
+    this.canvasCenterX = canvas.width/2;
+    this.canvasCenterY = canvas.height/2;
+    this.ctx = canvas.getContext('2d');
+
+    //polar coordinates
+    this.sphereRadius = canvas.width/3;
+    this.theta = Math.random() * 2 * Math.PI; //polar angle
+    this.phi = Math.acos((Math.random() * 2) -1); //azimuth angle
+
+    //initial 3D positon
+    this.xPos = 0;
+    this.yPos = 0;
+    this.zPos = 0;
+
+    //initial 2D position
+    this.xProjected = 0;
+    this.yProjected = 0;
+    this.depthProjected = 0;
+    
+  }
+
+  //project 3D position to 2D canvas
+  project3DTo2D = (multiplier) => {
+    this.xPos = multiplier * this.sphereRadius * Math.sin(this.phi) * Math.cos(this.theta);
+    this.yPos = multiplier * this.sphereRadius * Math.cos(this.phi);
+    this.zPos = multiplier * this.sphereRadius * Math.sin(this.phi) * Math.sin(this.theta) + this.sphereRadius;
+    
+      
+    //2D position of particle
+    this.depthProjected = this.perspective / (this.perspective + this.zPos);
+    this.xProjected = (this.xPos * this.depthProjected) + this.canvasCenterX;
+    this.yProjected = (this.yPos * this.depthProjected) + this.canvasCenterY;
+    if (this.depthProjected < 0){
+      this.depthProjected = this.depthProjected * -1
+    }
+  }
+
+  draw = (multiplier) => {
+    this.project3DTo2D(multiplier*.002); //get 2D position
+    this.ctx.globalAlpha = Math.abs(1 - this.z / this.canvas.width); //set opacity
+    this.ctx.fillStyle = this.color;
+    //draw particle on canvas
+    this.ctx.beginPath();
+    // this.ctx.strokeStyle = '#39FF14'
+    this.ctx.moveTo(this.canvasCenterX, this.canvasCenterY);
+    this.ctx.lineTo(this.xProjected, this.yProjected)
+    this.ctx.stroke();
+    
+    this.ctx.beginPath();
+    this.ctx.arc(
+      this.xProjected, 
+      this.yProjected, 
+      this.particleRadius * this.depthProjected,
+      0,
+      Math.PI * 2
+    );
+    this.ctx.fill();
+  }
+}
+
+
+export class SphereVisualizer{
+  constructor(canvas, numDots, multiplier){
+    this.canvas = canvas;
+    this.collection = [];
+    for ( let i = 0; i < numDots; i++ ){
+      this.collection.push(new SphereParticle(canvas, multiplier));
+    }
+  }
+
+  //will need to call window.requestAnimationFrame(renderDots)
+  renderParticles = (state) => {
+    this.collection.forEach((particle, i) => {
+      particle.draw(state.frequencyArray[i] * state.visualizerSettings.heightAmplifier)
+    })
+  }
+
+}
+
+export const drawSphere = (state, sphere) => {
+    // let sphere = new SphereVisualizer(canvas, 1000, multiplier);
+    sphere.renderParticles(state);
+}
