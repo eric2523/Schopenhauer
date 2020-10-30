69d80974089be935c5d10c399a947f3b1a37ed5f
eric2523@gmail.com <eric2523@gmail.com>
2020-10-29 22:20:52 -0700

    Add conditional render to toolbar

--- frontend/src/components/visualizers/visualizer.js
+++ frontend/src/components/visualizers/visualizer.js
@@ -3,56 +3,72 @@ import { CanvasWithRouter } from "./canvas";
 import { VisualizerSettings } from "./visualizer-settings";
 import { ToolbarItem } from "../toolbar/toolbar-item";
 import { testVisualizer } from "./test_visualizer_object";
-import { SongToolBar } from '../music_player/song_tool_bar';
+import { SongToolBar } from "../music_player/song_tool_bar";
 import { connect } from "react-redux";
 import { FrequencyVisualizer } from "./basic_frequency_visualizer";
 import { SphereVisualizer } from "./nate_visualizer_1";
 
 const mSTP = (state) => {
   return {
-    currentSong: state.session.song
+    currentSong: state.session.song,
     // id: state.session.song ? state.session.song.id : null
-  }
+  };
 };
 
-// EXAMPLE 
-{/* <VisualizerItemContainer visualizerSettings={visualerSettingsFromDB}/> */}
+// EXAMPLE
+/* <VisualizerItemContainer visualizerSettings={visualerSettingsFromDB}/> */
 
 class VisualizerItem extends React.Component {
   constructor(props) {
     super(props);
     switch (props.visualizerSettings.type) {
       case "frequency":
-        this.visualizer = new FrequencyVisualizer()
+        this.visualizer = new FrequencyVisualizer();
         break;
       case "sphere":
-        this.visualizer = new SphereVisualizer()
+        this.visualizer = new SphereVisualizer();
         break;
       default:
         break;
     }
-    this.visualizerSettings = props.visualizerSettings
+    this.visualizerSettings = props.visualizerSettings;
   }
 
   render() {
-    if (!this.props.currentSong || !Object.keys(this.props.currentSong).length) {
+    if (
+      !this.props.currentSong ||
+      !Object.keys(this.props.currentSong).length
+    ) {
       return null;
     }
-    const generalSettings = this.visualizerSettings.generalSettings;
-    let items = [];
-    for (const handle in generalSettings) {
-      let setting = {
-        name: handle,
-        val: generalSettings[handle],
-      };
-      items.push(
-        <ToolbarItem
-          key={handle}
-          generalSettings={this.visualizerSettings.generalSettings}
-          setting={setting}
-        />
+
+    let toolbar = null;
+    if (this.props.toolbox) {
+      let items = [];
+      const generalSettings = this.visualizerSettings.generalSettings;
+      for (const handle in generalSettings) {
+        let setting = {
+          name: handle,
+          val: generalSettings[handle],
+        };
+        items.push(
+          <ToolbarItem
+            key={handle}
+            generalSettings={this.visualizerSettings.generalSettings}
+            setting={setting}
+          />
+        );
+      }
+      toolbar = (
+        <>
+        <div className="toolbar">
+          <ul>{items}</ul>
+        </div>
+        <SongToolBar />
+        </>
       );
     }
+
     return (
       <div className="viz-outer-div">
         <div className="visualizer">
@@ -67,11 +83,7 @@ class VisualizerItem extends React.Component {
               />
             </div>
           </div>
-          <div className="toolbar">
-            <ul>{items}</ul>
-          </div>
-          {/* </div> */}
-          <SongToolBar />
+          {toolbar}
         </div>
       </div>
     );
