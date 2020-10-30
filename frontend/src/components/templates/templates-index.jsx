3c1b79de481b758f53b1b83f77636f1ec2efb93c
eric2523@gmail.com <eric2523@gmail.com>
2020-10-29 23:52:15 -0700

    Add index templates page

--- frontend/src/components/templates/templates-index.jsx
+++ frontend/src/components/templates/templates-index.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
-import { VisualizerItemContainer } from '../visualizers/visualizer'
-import { defaultFrequencySettings } from '../visualizers/basic_frequency_visualizer'
+import React from "react";
+import { VisualizerItemContainer } from "../visualizers/visualizer";
+import { defaultFrequencySettings } from "../visualizers/basic_frequency_visualizer";
+import { defaultSphereSettings } from "../visualizers/nate_visualizer_1";
+import { Link } from "react-router-dom";
 
-export const TemplatesIndex = (props) => {
-  return (
-    <div>
-      <VisualizerItemContainer visualizerSettings={defaultFrequencySettings}/>
-    </div>
-  )
+export class TemplatesIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="templates">
+        <div className="default-templates">
+          <ul>
+            <Link
+              to={{
+                pathname: "/visualizer/edit",
+                state: {
+                  visualizerSettings: defaultFrequencySettings,
+                },
+              }}
+            >
+              <li>
+                <h1>Frequency Visualizer</h1>
+                <VisualizerItemContainer
+                  canvasWidth={450}
+                  canvasHeight={450}
+                  toolbox={false}
+                  visualizerSettings={defaultFrequencySettings}
+                />
+              </li>
+            </Link>
+            <Link
+              to={{
+                pathname: "/visualizer/edit",
+                state: {
+                  visualizerSettings: defaultSphereSettings,
+                },
+              }}
+            >
+              <li>
+                <h1>Sphere Visualizer</h1>
+                <VisualizerItemContainer
+                  canvasWidth={450}
+                  canvasHeight={450}
+                  toolbox={false}
+                  visualizerSettings={defaultSphereSettings}
+                />
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
