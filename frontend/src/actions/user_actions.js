7faaa929613bdbae6dad751ca91b1bdae8688606
Nathan Chau <nateychau@gmail.com>
2020-10-29 18:45:00 -0700

    start on user show page

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -1,5 +1,7 @@
 export const RECEIVE_USER = "RECEIVE_USER";
 export const RECEIVE_ALL_USERS = "RECEIVE_ALL_USERS";
+// export const FOLLOW_USER = "FOLLOW_USER";
+// export const UNFOLLOW_USER = "UNFOLLOW_USER";
 
 const receiveUser = (user) => ({
   type: RECEIVE_USER,
@@ -10,3 +12,6 @@ const receiveAllUsers = (users) => ({
   type: RECEIVE_ALL_USERS,
   users
 })
+
+// const receiveFollow = ()
+
--- frontend/src/components/profile-page/profile-bio.jsx
+++ frontend/src/components/profile-page/profile-bio.jsx
@@ -1,4 +1,18 @@
 import React from 'react'
+import { connect } from 'react-redux';
+
+const mSTP = (state, ownProps) => {
+  return {
+    user: state.entities.users[ownProps.match.params.userId],
+    currentUser: state.session.user
+  }
+}
+
+const mDTP = (dispatch) => {
+  return {
+    
+  }
+}
 
 export class ProfileBio extends React.Component {
   constructor(props){
