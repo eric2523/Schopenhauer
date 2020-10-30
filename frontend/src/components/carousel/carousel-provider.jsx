6b2a6e2d948fb9f82a42b7976dc5bb8178a112f9
eric2523@gmail.com <eric2523@gmail.com>
2020-10-30 10:18:58 -0700

    Edit styling of carousel

--- frontend/src/components/carousel/carousel-provider.jsx
+++ frontend/src/components/carousel/carousel-provider.jsx
@@ -3,39 +3,58 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { CarouselItem } from "../landing-page/carousel-item";
+import { fetchUserVisualizer } from "../../actions/visualizer_actions";
+import { connect } from "react-redux";
 
-export const Carousel = (props) => {
-  const settings = {
-    className: "center",
-    centerMode: true,
-    infinite: true,
-    centerPadding: "0px",
-    slidesToShow: 3,
-    speed: 500,
-    dots: true,
-  };
-  return (
-    <div className="slide-wrap">
-      <Slider {...settings}>
-        <div className="slide-item-div">
-          <CarouselItem />
-        </div>
-        <div className="slide-item-div">
-          <CarouselItem />
-        </div>
-        <div className="slide-item-div">
-          <CarouselItem />
-        </div>
-        <div className="slide-item-div">
-          <CarouselItem />
-        </div>
-        <div className="slide-item-div">
-          <CarouselItem />
-        </div>
-        <div className="slide-item-div">
-          <CarouselItem />
-        </div>
-      </Slider>
-    </div>
-  );
-};
+const mSTP = (state) => ({
+  visualizers: state.entities.visualizers,
+  currUserId: state.session.user.id,
+});
+
+const mDTP = (dispatch) => ({
+  fetchUsersVisualizzers: (userId) => dispatch(fetchUserVisualizer(userId)),
+});
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchUsersVisualizzers(this.props.currUserId);
+  }
+
+  render() {
+    const settings = {
+      className: "center",
+      centerMode: true,
+      infinite: true,
+      centerPadding: "0px",
+      slidesToShow: 3,
+      speed: 500,
+      dots: true,
+    };
+
+    let visualizerKeys = Object.keys(this.props.visualizers);
+    let carouselItems = [];
+    if (visualizerKeys.length) {
+      carouselItems = visualizerKeys.map((key) => {
+        return (
+          <div className="slide-item-div">
+            <CarouselItem visualizerSettings={this.props.visualizers[visualizerKeys[key]]}/>
+          </div>
+        );
+      });
+    }
+
+    return (
+      <div className="slide-wrap">
+        <Slider {...settings}>
+          {carouselItems}
+        </Slider>
+      </div>
+    );
+  }
+}
+
+export const CarouselItemContainer = connect(mSTP, mDTP)(Carousel);
--- frontend/src/components/landing-page/carousel-item.jsx
+++ frontend/src/components/landing-page/carousel-item.jsx
@@ -5,19 +5,26 @@ import { testVisualizer } from "../visualizers/test_visualizer_object";
 export const CarouselItem = (props) => {
   // need to figure out what vars we want in the template
   // (name, time, etc)
+  let templateName = "Untitled"
+  // if (props.visualizerSettings.name){
+  //   templateName = props.visualizerSettings.name
+  // }
+
   return (
     <div className="carousel-item-div">
       <div className="carousel-item-title">
-        <h1 className="carousel-template-name">template name</h1>
+        <h1 className="carousel-template-name">
+          {templateName}
+        </h1>
       </div>
       <div className="carousel-item-visualizer">
-        {/* waiting for canvas.. */}
-        {/* <CanvasWithRouter canvasWidth={250} canvasHeight={250} /> */}
-        {/* <CanvasWithRouter
+        <CanvasWithRouter
           canvasWidth={250}
           canvasHeight={250}
-          visualizer={testVisualizer}
-        /> */}
+          toolbox={false}
+          // onHover={true}
+          visualizerSettings={props.visualizerSettings}
+        />
       </div>
       <div className="carousel-template-btn">
         <button className="ui primary button creation-temp-btn">go to creation</button>
