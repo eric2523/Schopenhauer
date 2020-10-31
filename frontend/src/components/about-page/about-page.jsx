63119d1ee2c3e012369ae5886932d5b750fd0cfb
eric2523@gmail.com <eric2523@gmail.com>
2020-10-31 15:44:21 -0700

    Add about-page skeleton and splash img

---
+++ frontend/src/components/about-page/about-page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export class AboutPage extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div>
+        <div className="about-splash"></div>
+        About Page
+      </div>
+    )
+  }
+}
