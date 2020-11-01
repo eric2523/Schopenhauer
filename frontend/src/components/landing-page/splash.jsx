e7743230d28e62bd1d527344941a06ca1af684ac
Yuehan <yuehan.huang@protonmail.com>
2020-10-31 19:12:32 -0700

    Add music

--- frontend/src/components/landing-page/splash.jsx
+++ frontend/src/components/landing-page/splash.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ConnectedFloatingDotsVisualizer } from "../visualizers/visualizer_templates/floating_particles_with_connection";
-
+import song from "../../audio_files/bensound-goinghigher.mp3";
+import { BeatDetection } from "../visualizers/beat_detection";
 export class Splash extends React.Component {
   constructor(props) {
     super(props);
     this.canvas = React.createRef();
-
+    const binCount = 1024;
+    this.audio = new Audio();
+    this.audio.crossOrigin = "anonymous";
+    this.audio.src = song;
     this.state = {
       mouse: {
         x: 0,
@@ -15,13 +19,21 @@ export class Splash extends React.Component {
       },
       visualizer: null,
       rafId: null,
+      play: false,
+      beatDetection: new BeatDetection(),
+      audioContext: null,
+      source: null,
+      analyser: null,
+      frequencyArray: new Uint8Array(binCount),
+      binCount,
     };
     this.onMouseMove = this.onMouseMove.bind(this);
     this.tick = this.tick.bind(this);
+    this.togglePlay = this.togglePlay.bind(this);
+    this.updateFrequencyData = this.updateFrequencyData.bind(this);
   }
 
   componentDidMount() {
-    debugger;
     this.bound = this.canvas.current.getBoundingClientRect();
     this.canvas.current.height = window.innerHeight - this.bound.top; // 10 is for the padding of the immediate sibling element
     this.canvas.current.width = window.innerWidth - this.bound.left;
@@ -34,8 +46,13 @@ export class Splash extends React.Component {
   }
 
   componentWillUnmount() {
+    this.audio.pause();
     cancelAnimationFrame(this.state.rafId);
+    this.setState({
+      play: false,
+    });
   }
+
   onMouseMove(e) {
     this.setState({
       mouse: {
@@ -51,15 +68,66 @@ export class Splash extends React.Component {
       },
     });
   }
+
+  togglePlay() {
+    // checks if audio input is in (can change second conditional later to be more specific. Currently just a placeholder until I figure out a better flag )
+    if (this.audio instanceof Audio && !this.state.source) {
+      const audioContext = new (window.AudioContext ||
+        window.webkitAudioContext)();
+      const source = audioContext.createMediaElementSource(this.audio);
+      const analyser = audioContext.createAnalyser();
+      this.setState({
+        audioContext,
+        source,
+        analyser,
+      });
+    }
+    if (!this.state.play) {
+      this.audio.play();
+      let rafId = requestAnimationFrame(this.tick);
+      this.setState({
+        rafId,
+        play: true,
+      });
+    } else {
+      this.audio.pause();
+      cancelAnimationFrame(this.state.rafId);
+      this.setState({
+        play: false,
+      });
+    }
+  }
+  updateFrequencyData() {
+    this.state.analyser.getByteFrequencyData(this.state.frequencyArray);
+    this.state.beatDetection.update(this.state.frequencyArray);
+  }
   animation(canvas) {
     this.state.visualizer.animate(canvas, this.state);
   }
+
   tick() {
     this.animation(this.canvas.current);
     this.setState({ rafId: requestAnimationFrame(this.tick) });
   }
 
   render() {
+    if (this.state.source && this.state.analyser) {
+      //this needs to be hook up to a speaker
+      if (this.props.disconnectMusic) {
+        this.state.source.connect(this.state.analyser);
+        // need testing seems redundant
+        this.state.analyser.connect(this.state.audioContext.destination);
+        this.state.analyser.disconnect(this.state.audioContext.destination);
+      } else {
+        this.state.source.connect(this.state.analyser);
+        this.state.analyser.connect(this.state.audioContext.destination);
+      }
+    }
+    const buttonText = !this.state.play ? (
+      <i className="play icon white-audio-icon"></i>
+    ) : (
+      <i className="pause icon white-audio-icon"></i>
+    );
     return (
       <div className="splash-outer-div">
         <canvas
@@ -67,6 +135,9 @@ export class Splash extends React.Component {
           ref={this.canvas}
           onMouseMove={this.onMouseMove}
         ></canvas>
+        <button className="carousel-play" onClick={this.togglePlay}>
+          {buttonText}
+        </button>
         <div className="splash-title">
           <div className="main-title">
             <h1 className="splash-title">Schopenhauer</h1>
