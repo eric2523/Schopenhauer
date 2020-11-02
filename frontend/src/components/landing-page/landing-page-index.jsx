55a7d29dd5f2d0bbe4e377d410d52429644e158c
Nathan Chau <nateychau@gmail.com>
2020-11-02 00:09:18 -0800

    Add functionality to bring up modal when user attempts to visit protected routes

--- frontend/src/components/landing-page/landing-page-index.jsx
+++ frontend/src/components/landing-page/landing-page-index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { CarouselIndex } from "./carousel";
-import { Splash } from "./splash";
+import { Splash, SplashContainer } from "./splash";
 
 export const LandingPageIndex = (props) => {
   return (
     <div>
-      <Splash />
+      <SplashContainer />
       {/* <CarouselIndex /> */}
     </div>
   );
--- frontend/src/components/nav-bar/nav-bar-item.jsx
+++ frontend/src/components/nav-bar/nav-bar-item.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { openModal } from "../../actions/modal_actions";
 
-export class NavBarItem extends React.Component {
+export class NavBarItemTEST extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    if (!this.props.loggedIn && this.props.name !== 'about'){
+      return (
+        <div className="nav-redirect" onClick={this.props.openModal}>
+          <NavLink
+            className="nav-link-li"
+            activeClassName="active-blue"
+            to={this.props.path}
+          >
+            <li className="navbar-item-li">{this.props.name}</li>
+          </NavLink>
+        </div>
+      )
+    }
     return (
       <NavLink
         className="nav-link-li"
@@ -18,3 +33,17 @@ export class NavBarItem extends React.Component {
     );
   }
 }
+
+const mSTP = (state) => {
+  return {
+    loggedIn: Object.keys(state.session.user).length
+  }
+}
+
+const mDTP = (dispatch) => {
+  return {
+    openModal: () => dispatch(openModal('login'))
+  }
+}
+
+export const NavBarItem = connect(mSTP, mDTP)(NavBarItemTEST)
