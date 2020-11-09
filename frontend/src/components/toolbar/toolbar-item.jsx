a744078f4f052ed1ed4e4c50f3192713ce385aeb
eric2523@gmail.com <eric2523@gmail.com>
2020-11-09 11:43:58 -0800

    Add color hex code to general settings

--- frontend/src/components/toolbar/toolbar-item.jsx
+++ frontend/src/components/toolbar/toolbar-item.jsx
@@ -4,12 +4,22 @@ export const ToolbarItem = (props) => {
   return (
     <div className="toolbar-item-container">
       <div className="ui transparent input toolbar-input-container">
-        <input
-          onChange={props.handleChange(props.setting.name)}
-          type="number"
-          placeholder={props.setting.name}
-        />
+        {props.setting.name !== "color" ? (
+          <input
+            onChange={props.handleChange(props.setting.name)}
+            type="number"
+            placeholder={props.setting.name}
+            value={props.setting.val}
+          />
+        ) : (
+          <input
+            onChange={props.handleChange(props.setting.name)}
+            type="text"
+            placeholder={props.setting.name}
+            value={props.setting.val}
+          />
+        )}
       </div>
     </div>
   );
-}
+};
