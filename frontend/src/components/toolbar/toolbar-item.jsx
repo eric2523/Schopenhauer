3e618eccbbee0c5d25f82b0392e000f7cc963644
eric2523@gmail.com <eric2523@gmail.com>
2020-11-09 12:01:31 -0800

    Add parseString function

--- frontend/src/components/toolbar/toolbar-item.jsx
+++ frontend/src/components/toolbar/toolbar-item.jsx
@@ -1,21 +1,41 @@
 import React from "react";
 
+const parseString = (str) => {
+  let words = [];
+  let startIdx = 0;
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i] 
+    if (char === char.toUpperCase()){
+      words.push(str.slice(startIdx, i))
+      startIdx = i;
+    }
+    if (i === str.length - 1){
+      debugger
+      words.push(str.slice(startIdx, i + 1))
+    }
+  }
+  return words.join(" ").toLowerCase();
+}
+
 export const ToolbarItem = (props) => {
+  const parsedString = parseString(props.setting.name)
+
   return (
     <div className="toolbar-item-container">
+      <h1>{parsedString}</h1>
       <div className="ui transparent input toolbar-input-container">
         {props.setting.name !== "color" ? (
           <input
             onChange={props.handleChange(props.setting.name)}
             type="number"
-            placeholder={props.setting.name}
+            placeholder={parsedString}
             value={props.setting.val}
           />
         ) : (
           <input
             onChange={props.handleChange(props.setting.name)}
             type="text"
-            placeholder={props.setting.name}
+            placeholder={parsedString}
             value={props.setting.val}
           />
         )}
